4fd6e5eba1c32fb8d64463716316f03d9560f249
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-05-27 14:08:39 +0200

    * some typos

--- chemical_composition/chemical_composition.py
+++ chemical_composition/chemical_composition.py
@@ -21,7 +21,7 @@ class ChemicalComposition(dict):
     Keyword Arguments:
         sequence (str): peptide sequence that should be used for the instance, e.g. "PEPTIDE"
         formula (str|dict): chemical formula(s) that will be added/subtracted from the instance, e.g. "-H2O".
-            The formula can also be given as a dictionary defining the compsition, e.g. {H:2, "O":1}
+            The formula can also be given as a dictionary defining the compsition, e.g. {"H":2, "O":1}
         modifications (str): modifications of the peptide sequence, given as "Unimod:pos", e.g. "Oxidation:2"
         glycan (str): glycan composition that will be added to the instance, e.g. "HexNAc(2)Hex(5)"
         deprecated_format (str): old input format, using either "PEPTIDE+Formula-Formula" or "PEPTIDE#Unimod:pos"
@@ -397,10 +397,10 @@ class ChemicalComposition(dict):
             formula (str|dict): chemical formula in hill notation (or unimod hill notation).
                 Plus (+) and minus (-) are interpreted to add and subtract, respectively, the formula.
                 If the string does not start with +/-, the default is to add the formula.
-                If the formula is given as a dict, the keys should be integers and thereby define addition
-                or subtraction of the elements.
+                If the formula is given as a dict, the values should be integers and thereby define
+                addition or subtraction of the elements.
         """
-        if type(formula) is dict:
+        if isinstance(formula, dict):
             self._merge(formula)
             return
         elif "(" in formula:
@@ -570,7 +570,8 @@ class ChemicalComposition(dict):
         """
         if self._unimod_parser is None:
             self._unimod_parser = unimod_mapper.UnimodMapper(
-                xml_file_list=self.unimod_files, add_default_files=self.add_default_files
+                xml_file_list=self.unimod_files,
+                add_default_files=self.add_default_files,
             )
         modification_list = []
         if self.modifications is not None:
